aa914d39a05b3b69685ce8513bb563d6583474d0
unknown <davi_nogueira@dominio.ufms.br>
2022-06-09 08:24:56 -0400

    Separação do inicio e  final do html chamando php

--- src/Controller/FormularioInsercao.php
+++ src/Controller/FormularioInsercao.php
@@ -6,6 +6,7 @@ class FormularioInsercao implements InterfaceControladorRequisicao
 {
     public function  processaRequisicao() : void
     {
+        $titulo = "Novo curso";
       require_once __DIR__ . "/../../view/cursos/formulario.php";
     }
 }
